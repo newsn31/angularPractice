b0f1aefdc151e452b516ca3a1a88e8c411cc17e1
quang <quang.vo31@gmail.com>
2015-06-19 18:36:13 -0400

    ng-source confusing

--- ShapingUpWithAngular/2.1/app.js
+++ ShapingUpWithAngular/2.1/app.js
@@ -8,6 +8,16 @@
     name: 'Dodecahedron',
     price: 2.95,
     description: 'This is the description of the Dodecahedron.',
+    images: [
+      {
+        full: 'dodecahedron-01-full.jpg',
+        thumb: 'dodecahedron-01-thumb.jpg'
+      },
+      {
+        full: 'dodecahedron-02-full.jpg',
+        thumb: 'dodecahedron-02-thumb.jpg'
+      }
+    ],
     canPurchase: true,
     soldOut: false,
   },
